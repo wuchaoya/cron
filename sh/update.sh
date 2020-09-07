4ddd949107d0b45b4035340387de481bc67448c9
wuchao <wuchao@haima.me>
2020-09-07 16:16:04 +0800

    修改sh中.env路径

--- sh/update.sh
+++ sh/update.sh
@@ -1,1 +1,1 @@
-docker run -it --env-file=.env -e "CONFIG=$(cat /root/config/config.json | jq -r tostring)" algolia/docsearch-scraper
+docker run -it --env-file=/root/.env -e "CONFIG=$(cat /root/config/config.json | jq -r tostring)" algolia/docsearch-scraper
